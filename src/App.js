34b6eb4c19b298829563218e2588d79c6d2fa5c9
DESKTOP-NGISFV2\galih <galihfw23@gmail.com>
2023-01-02 02:21:58 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useMemo, useState } from "react";
+export default function App() {
+  const [word, setWord] = useState("");
+  const isPalindrome = useMemo(() => {
+    return word === word.split("").reverse().join("");
+  }, [word]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div>
+          <h1>Palindrome Checker</h1>
+          <p>
+            "992299’", "12291", 982289, 22022022, 2301, 2013, 1001, 756564,
+            1011, 766567, 756546, 2002, 91019, 765567,
+          </p>
+          <label>word to check</label>
+          <input value={word} onChange={(e) => setWord(e.target.value)} />
+        </div>
+      </form>
+      <div>Is Palindrome:{isPalindrome ? "Yes" : "No"}</div>
+      <div>
+        <p>Palindrome : 992299,982289,22022022,1001,2002,91019,765567</p>
+      </div>
     </div>
   );
 }
-
-export default App;
